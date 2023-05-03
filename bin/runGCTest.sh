58eb54b0f74dca2d26fffb24269da34fb1aed949
CoralBlocks <132032478+coralblocks@users.noreply.github.com>
2023-05-03 15:46:52 -0300

    Reduce the heap size for more gc if garbage is being created

--- bin/runGCTest.sh
+++ bin/runGCTest.sh
@@ -3,7 +3,7 @@
 PRODUCE_GARBAGE=${1:-false}
 ITERATIONS=${2:-1000000}
 
-CMD="java -verbose:gc -Xms128m -Xmx256m -cp target/classes com.coralblocks.coralme.example.NoGCTest $PRODUCE_GARBAGE $ITERATIONS"
+CMD="java -verbose:gc -Xms32m -Xmx64m -cp target/classes com.coralblocks.coralme.example.NoGCTest $PRODUCE_GARBAGE $ITERATIONS"
 
 echo $CMD
 
