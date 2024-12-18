2a495d802f9785842e9841bf40587cbfda0fdbe6
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-12-18 16:35:12 -0300

    Fix header license

--- src/main/java/com/coralblocks/coralme/util/LongMap.java
+++ src/main/java/com/coralblocks/coralme/util/LongMap.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2023 (c) CoralBlocks - http://www.coralblocks.com
+ * Copyright 2015-2024 (c) CoralBlocks LLC - http://www.coralblocks.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
