ca630c80d1532269695a417e199c0e1f4362605a
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2026-08-20 00:26:03 -0300

    Organize imports

--- src/test/java/com/coralblocks/coralme/ListenerDifferentOrderBookMutationTest.java
+++ src/test/java/com/coralblocks/coralme/ListenerDifferentOrderBookMutationTest.java
@@ -15,8 +15,7 @@
  */
 package com.coralblocks.coralme;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.Collection;
--- src/test/java/com/coralblocks/coralme/OrderListenerExceptionsTest.java
+++ src/test/java/com/coralblocks/coralme/OrderListenerExceptionsTest.java
@@ -15,11 +15,7 @@
  */
 package com.coralblocks.coralme;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
